872ce8941654c4d28dbb3ba55c782de4d7b94ef1
Simon Danisch <sdanisch@gmail.com>
2016-12-21 18:37:40 +0100

    enable build on windows without cl.exe

--- deps/build.jl
+++ deps/build.jl
@@ -1,22 +1,40 @@
 #! /usr/bin/env julia
 
-pkg_dir = dirname(dirname(@__FILE__))
+path = joinpath(dirname(@__FILE__), "..", "src")
 
-@static if is_windows()
-    try
-      run(`where cl.exe`)
-    catch
-      error("ERROR: Could not find cl.exe.")
+if is_windows()
+    if success(`where cl.exe`)
+        info("Compiling with cl.exe")
+        run(`cl.exe /D_USRDLL /D_WINDLL /EHsc /Fo$(path) $(path)cclipper.cpp $(path)clipper.cpp /MT /link /DLL /OUT:$(path)cclipper.dll`)
+    else
+        import WinRPM
+        info("Compiling with WinRPM gcc-c++")
+        WinRPM.install("gcc-c++"; yes = true)
+        WinRPM.install("gcc"; yes = true)
+        WinRPM.install("headers"; yes = true)
+
+        gpp = Pkg.dir("WinRPM","deps","usr","x86_64-w64-mingw32","sys-root","mingw","bin","g++")
+        RPMbindir = Pkg.dir("WinRPM","deps","usr","x86_64-w64-mingw32","sys-root","mingw","bin")
+        incdir = Pkg.dir("WinRPM","deps","usr","x86_64-w64-mingw32","sys-root","mingw","include")
+
+        push!(Base.Libdl.DL_LOAD_PATH, RPMbindir)
+        ENV["PATH"] = ENV["PATH"] * ";" * RPMbindir
+        if success(`$gpp --version`)
+            cd(path) do
+                run(`$gpp -c -fPIC -std=c++11 clipper.cpp cclipper.cpp -I $incdir`)
+                run(`$gpp -shared -o cclipper.dll clipper.o cclipper.o`)
+            end
+        else
+            error("no windows c++ compiler (cl.exe) found, and WinRPM with g++ is failing as well.")
+        end
     end
-
-    path = "$(pkg_dir)\\src\\"
-    run(`cl.exe /D_USRDLL /D_WINDLL /EHsc /Fo$(path) $(path)cclipper.cpp $(path)clipper.cpp /MT /link /DLL /OUT:$(path)cclipper.dll`)
 end
 
-@static if is_unix()
-    cd("$(pkg_dir)/src") do
+if is_unix()
+    cd(path) do
         # Note: on Mac OS X, g++ is aliased to clang++.
         run(`g++ -c -fPIC -std=c++11 clipper.cpp cclipper.cpp`)
         run(`g++ -shared -o cclipper.so clipper.o cclipper.o`)
     end
 end
+
