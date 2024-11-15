ae2fd7fea4c53dac8f5170855258de1d1abf7aa1
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-11-15 09:31:26 -0500

    Fix docs generation

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ using Clipper
 makedocs(
     # See https://github.com/JuliaDocs/Documenter.jl/issues/868
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    strict = true,
     sitename = "Clipper",
     pages = [
         "Home" => "index.md",
